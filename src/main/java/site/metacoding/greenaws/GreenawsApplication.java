80ebde14c01fddf3b0bb064a9afdd04a3970cefe
hheeju98 <hheeju98@naver.com>
2022-04-09 14:58:06 +0900

    프로젝트

---
+++ src/main/java/site/metacoding/greenaws/GreenawsApplication.java
@@ -0,0 +1,13 @@
+package site.metacoding.greenaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenawsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenawsApplication.class, args);
+	}
+
+}
